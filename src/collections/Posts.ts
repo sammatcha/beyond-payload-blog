28274c5477cbd8bab0d7d26143c65830312beb8f
Samantha <samantha@uppointinc.com>
2025-02-05 01:06:54 -0800

    initial commit and test post

---
+++ src/collections/Posts.ts
@@ -0,0 +1,21 @@
+import type { CollectionConfig } from 'payload'
+
+export const Posts: CollectionConfig = {
+  slug: 'posts',
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+      required: true,
+    },
+  ],
+  upload: true,
+}
